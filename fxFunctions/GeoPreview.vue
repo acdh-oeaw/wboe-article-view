72dca4e6209498fec4f668c152bfe76c31e3b299
arnold-graf <arnold.graf@gmail.com>
2020-05-15 19:42:15 +0200

    WIP: append großregionen to gemeinden

--- fxFunctions/GeoPreview.vue
+++ fxFunctions/GeoPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <span :id="'gs' + content.uId"><template v-for="(place, pKey) in placesView()"><template v-if="pKey > 0">{{ content.fxData.join }}</template>{{ place.orgXmlObj.getValue(false) }}</template></span>
+  <span :id="'gs' + content.uId"><template v-for="(place, pKey) in placesView()"><template v-if="pKey > 0">{{ content.fxData.join }}</template>{{ place.orgXmlObj.getValue(false) + maybeGrossregion(place) }}</template></span>
 </template>
 
 <script>
@@ -18,17 +18,38 @@
     watch: {
     },
     mounted () {
+      console.log(this.content, this.placesView())
     },
     methods: {
       placesView () {
-        let aPlaces = []
-        this.content.getChilds('all', true).forEach(function (child) {
-          if (child.orgXmlObj.name === 'placeName' && child.orgXmlObj.getValue(false)) {
-            aPlaces.push(child)
-          }
-        }, this)
-        return aPlaces
+        return this.content.getChilds('all', true).filter(c => {
+          return c.orgXmlObj.name === 'placeName' && c.orgXmlObj.getValue(false)
+        })
       },
+      last (array) {
+        return array[array.length - 1]
+      },
+      getPlacenameSigleFromRef (ref) {
+        if (ref === null) {
+          return null
+        } else {
+          return this.last(ref.split(/([#p])/)) || null
+        }
+      },
+      maybeGrossregion (place) {
+        if (
+          place.orgXmlObj !== undefined &&
+          place.orgXmlObj.attributes !== undefined &&
+          place.orgXmlObj.attributes.ref !== undefined && 
+          place.orgXmlObj.attributes.type === 'gemeinde'
+        ) {
+          // TODO: resolve the Großregion
+          // return this.getPlacenameSigleFromRef(place.orgXmlObj.attributes.ref) || ''
+          return ''
+        } else {
+          return ''
+        }
+      }
     },
     components: {
     },
